4151ff2324736d3ea6e80163d10bf5cfd7b73e22
Tom Nguyen <nguyentom1996@yahoo.com>
2018-03-12 22:52:30 -0600

    Added a Boggle architecture

--- Boggle/Boggle/Boggle.cs
+++ Boggle/Boggle/Boggle.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Boggle
 {
-    public partial class Form1 : Form
+    public partial class Boggle : Form
     {
-        public Form1()
+        public Boggle()
         {
             InitializeComponent();
         }
