08c06524234d7a04900a00cbc8505cc3645e0ab9
Tom Nguyen <nguyentom1996@yahoo.com>
2018-03-13 15:49:25 -0600

    add action event

--- Boggle/Boggle/BoggleView.cs
+++ Boggle/Boggle/BoggleView.cs
@@ -8,5 +8,22 @@ namespace Boggle
 {
     interface BoggleView
     {
+
+
+
+
+
+        event Action<string> RegisterUser;
+
+        event Action CancelRegisterUser;
+
+        event Action CancelGame;
+
+        event Action RequestGame;
+
+        event Action<string> SubmitPlayWord;
+
+        event Action ClearPlayWord;
+
     }
 }
