66edfe59d3abfbd424c7b8c7efd8c3eac31d44a5
Tom Nguyen <nguyentom1996@yahoo.com>
2018-03-27 18:31:04 -0600

    Interface done, data model about to revise.

--- Spreadsheet/BoggleService/BoggleService/IBoggleService.cs
+++ Spreadsheet/BoggleService/BoggleService/IBoggleService.cs
@@ -15,11 +15,43 @@ namespace Boggle
         Stream API();
 
         /// <summary>
-        /// Returns the nth word from dictionary.txt.  If there is
-        /// no nth word, responds with code 403. This is a demo;
-        /// you can delete it.
+        /// Create User
         /// </summary>
-        [WebGet(UriTemplate = "/word?index={n}")]
-        string WordAtIndex(int n);
+        /// <param name="nickName"></param>
+        /// <returns></returns>
+        [WebInvoke(Method = "POST", UriTemplate = "/users")]
+        string CreateUser (string Nickname);
+
+        /// <summary>
+        /// Join Game
+        /// </summary>
+        /// <param name="UserToken"></param>
+        /// <param name="TimeLimit"></param>
+        /// <returns></returns>
+        [WebInvoke(Method = "POST", UriTemplate = "/games")]
+        int JoinGame (string UserToken, int TimeLimit);
+
+        /// <summary>
+        /// Cancel Join Request
+        /// </summary>
+        /// <param name="UserToken"></param>
+        [WebInvoke(Method = "PUT", UriTemplate = "/games")]
+        void CancelJoinRequest (string UserToken);
+
+        /// <summary>
+        /// Play word
+        /// </summary>
+        /// <param name="UserToken"></param>
+        /// <param name="Word"></param>
+        [WebInvoke(Method = "PUT", UriTemplate = "/games/{GameID}")]
+        string PlayWord (string UserToken, string Word);
+
+        /// <summary>
+        /// Getting the game status
+        /// </summary>
+        /// <param name="Brief"></param>
+        /// <returns></returns>
+        [WebInvoke(Method = "GET", UriTemplate = "/games/{GameID}")]
+        string GetGameStatus (string Brief);
     }
 }
